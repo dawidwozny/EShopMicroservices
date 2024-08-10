2d042b9bd6173c74e40e7ccb020c1ef47914a061
Dawid Wozny <dawid.wozny@zoryatec.com>
2024-08-10 18:57:09 +0200

    224

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,27 @@
+using Ordering.Application;
+using Ordering.Infrastracture;
+using Ordering.API;
+using Ordering.Infrastructure.Data.Extensions; 
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// add services to the container
+builder.Services
+                .AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration)
+                .AddApiServices();
+
+
+var app = builder.Build();
+
+// configure http pipeline
+app.UseApiServices();
+
+if(app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
+
+app.Run();
